30f669d0c9e68cfc10dfb2fd42778427d2655ac3
Andrey Shitikov <andrey.shitikoff@gmail.com>
2020-09-24 11:03:44 +0300

    Fields passed to method calculate

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,20 @@ public class Main {
     public static void main(String[] arg) {
 
         double percent = 9.99;
-        double term = 12;
+        double termOneYear = 12;
+        double termTwoYear = 24;
+        double termThreeYear = 36;
         double sum = 1_000_000;
 
         CreditPaymentService service = new CreditPaymentService();
 
-        double monthlyPaymentOneYear = service.calculate(12,1_000_000,9.99);
+        double monthlyPaymentOneYear = service.calculate(termOneYear, sum, percent);
         System.out.println(monthlyPaymentOneYear);
 
-        double monthlyPaymentTwoYear = service.calculate(24,1_000_000, 9.99);
+        double monthlyPaymentTwoYear = service.calculate(termTwoYear, sum, percent);
         System.out.println(monthlyPaymentTwoYear);
 
-        double monthlyPaymentThreeYear = service.calculate(36, 1_000_000, 9.99);
+        double monthlyPaymentThreeYear = service.calculate(termThreeYear, sum, percent);
         System.out.println(monthlyPaymentThreeYear);
 
     }
